ea453b4f7342285567254d1b6d55be61b3e1c321
snw380 <ty.morgan818@gmail.com>
2020-09-29 16:59:30 -0400

    changed up the get method for api cod

--- routes/api-cod.js
+++ routes/api-cod.js
@@ -1,16 +1,15 @@
-$(function () {
-    var queryURL = "https://call-of-duty-modern-warfare.p.rapidapi.com/multiplayer/" + gamertag + "%2523" + gamertagNum + "/" + platform;
-    // ------- these var's are temporary -------
-    var gamertag = "Sunshine"
-    var gamertagNum = "14256"
-    var platform = "battle"
-    // -----------------------------------------
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (res) {
-        alert("Load was performed.");
-        console.log(res)
-        console.log(res.Runtime)
-    });
+const settings = {
+  "url": "https://call-of-duty-modern-warfare.p.rapidapi.com/multiplayer/Sunshine%252314256/battle",
+  "method": "GET",
+  "timeout": 0,
+  "headers": {
+    "x-rapidapi-host": "call-of-duty-modern-warfare.p.rapidapi.com",
+    "x-rapidapi-key": "2dc88bc862msh01a6273fcbf4194p1e37acjsn0de8a177d74a",
+    "content-type": "application/json",
+    "useQueryString": "true"
+  },
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
 });
