5f2e125b8e0245009ef40ce66502c14cab8b706e
snw380 <ty.morgan818@gmail.com>
2020-10-01 00:14:02 -0400

    css is now being applied when server is running

--- public/js/members.js
+++ public/js/members.js
@@ -2,10 +2,10 @@ $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
-    $(".member-name").text(data.email);
+    $(".Gamertag").text(data.gamertag);
   });
 
-  $.get("/api/user_data/stats").then(data => {
-    $(".user-stats").text(data.kdRatio)
-  })
+  // $.get("/api/user_data/stats").then(data => {
+  //   $(".user-stats").text(data.kdRatio)
+  // })
 });
